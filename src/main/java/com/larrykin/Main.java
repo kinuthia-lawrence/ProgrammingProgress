c5c0bd8f50dc53b77dcd84f51b4baa874a95b22e
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-05-21 15:29:33 +0300

    added the sqlite database

--- src/main/java/com/larrykin/Main.java
+++ src/main/java/com/larrykin/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -15,6 +16,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/FXML/Login.fxml"));
         primaryStage.setTitle("Programming Progress Tracker");
+        primaryStage.setResizable(false);
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
+
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
