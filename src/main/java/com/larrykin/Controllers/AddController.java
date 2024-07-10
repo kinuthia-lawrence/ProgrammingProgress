90cb7efaf9cd4ff4d517b10c32d6f31eb7de3e3e
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-07-10 21:34:12 +0300

    Created the add project UI

---
+++ src/main/java/com/larrykin/Controllers/AddController.java
@@ -0,0 +1,41 @@
+package com.larrykin.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class AddController {
+
+    @FXML
+    private AnchorPane addAnchorPane;
+
+    @FXML
+    private DatePicker datePicker;
+
+
+    @FXML
+    private TextArea futureImprovementTextArea;
+
+    @FXML
+    private ComboBox<?> languageComboBox;
+
+    @FXML
+    private ComboBox<?> milestoneComboBox;
+
+    @FXML
+    private TextArea milestoneDescription;
+
+    @FXML
+    private TextArea projectDescriptionTextArea;
+
+    @FXML
+    private TextField projectNameTextField;
+
+    @FXML
+    private Button saveButton;
+
+}
