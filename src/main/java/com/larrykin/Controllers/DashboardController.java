358c0681beda140b6cdd27d73eb4a161107a79c9
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-07-10 20:33:22 +0300

    modified the dashboard page

--- src/main/java/com/larrykin/Controllers/DashboardController.java
+++ src/main/java/com/larrykin/Controllers/DashboardController.java
@@ -1,26 +1,35 @@
 package com.larrykin.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.fxml.FXML;
-
-import java.awt.*;
 
 public class DashboardController {
+
     @FXML
-    public HBox headerPane1;
+    private BorderPane BorderPane;
+
     @FXML
-    public AnchorPane titleAnchorPane;
-    @FXML
-    public javafx.scene.layout.BorderPane BorderPane;
+    private Button addDashboardButton;
+
     @FXML
     private AnchorPane centerPane;
+
+    @FXML
+    private Button editDashboardButton;
+
     @FXML
     private HBox footerPane;
 
     @FXML
-    private HBox headerPane;
+    private HBox headerPane1;
 
     @FXML
     private VBox leftPane;
@@ -28,5 +37,43 @@ public class DashboardController {
     @FXML
     private AnchorPane logoImage;
 
+    @FXML
+    private ImageView notificationBellIcon;
+
+    @FXML
+    private Button notificationButton;
+
+    @FXML
+    private Label notificationCountLabel;
+
+    @FXML
+    private Button otherDashboardButton;
+
+    @FXML
+    private Button projectsDashboardButton;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private ComboBox<?> searchComboBox;
+
+    @FXML
+    private HBox searchPane;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private AnchorPane titleAnchorPane;
+
+    @FXML
+    private Label titleAnchorPane1;
+
+    @FXML
+    private Button todoDashboardButton;
+
+    @FXML
+    private Button viewDashboardButton;
 
 }
