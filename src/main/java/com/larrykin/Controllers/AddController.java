4e3cdea283ff18be9d24185998a9a5ab0da56b99
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-07-18 16:49:22 +0300

    added functionality to check fields

--- src/main/java/com/larrykin/Controllers/AddController.java
+++ src/main/java/com/larrykin/Controllers/AddController.java
@@ -1,15 +1,12 @@
 package com.larrykin.Controllers;
 
+import com.larrykin.Utils.ComboBoxUtils;
 import com.larrykin.Utils.DatabaseConn;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -49,30 +46,30 @@ public class AddController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Populate the comboboxes
-        populateLanguageComboBox();
-        populateMilestoneComboBox();
+        // Populate the comboboxes using the utility class
+        ComboBoxUtils.populateLanguageComboBox(languageComboBox);
+        ComboBoxUtils.populateMilestoneComboBox(milestoneComboBox);
+        saveButton.setOnAction(actionEvent -> saveButtonAction());
     }
 
-    private void populateMilestoneComboBox() {
-        ObservableList<String> milestones = FXCollections.observableArrayList(
-                "TODO ", "UX/UI", "SETUP ENVIRONMENT", "IN PROGRESS", "DONE"
-        );
-        milestoneComboBox.setItems(milestones);
+
+    public void saveButtonAction() {
+        //check if fields are empty
+        if (!projectNameTextField.getText().isBlank() && !projectDescriptionTextArea.getText().isBlank() && !milestoneDescription.getText().isBlank() && !futureImprovementTextArea.getText().isBlank() && datePicker.getValue() != null && languageComboBox.getValue() != null && milestoneComboBox.getValue() != null) {
+            addProject();
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Empty Fields");
+            alert.setContentText("Please fill in all fields");
+            alert.showAndWait();
+        }
     }
 
-    private void populateLanguageComboBox() {
-        ObservableList<String> languages = FXCollections.observableArrayList(
-                "Java", "C", "C++", "Python", "JavaScript"
-        );
-        languageComboBox.setItems(languages);
-    }
-
-    public  void saveButtonAction(){
+    private void addProject() {
         //create a new instance of the database connection
         DatabaseConn connectNow = new DatabaseConn();
         Connection connectDB = connectNow.getConnection();
-
     }
 
 
---
+++ src/main/java/com/larrykin/Utils/ComboBoxUtils.java
@@ -0,0 +1,22 @@
+package com.larrykin.Utils;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+
+public class ComboBoxUtils {
+
+    public static void populateLanguageComboBox(ComboBox<String> comboBox) {
+        ObservableList<String> languages = FXCollections.observableArrayList(
+                "Language", "Java", "React", "PHP", "Kotlin", "Flutter", "Python", "JavaScript"
+        );
+        comboBox.setItems(languages);
+    }
+
+    public static void populateMilestoneComboBox(ComboBox<String> comboBox) {
+        ObservableList<String> milestones = FXCollections.observableArrayList(
+                "Milestone", "TODO", "UX/UI", "SETUP ENVIRONMENT", "IN PROGRESS", "DONE"
+        );
+        comboBox.setItems(milestones);
+    }
+}
