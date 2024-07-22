c7c215f190ff91b68ecce0e81747d6a870bc97e3
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-07-22 08:41:04 +0300

    handle populate edit nodes

--- src/main/java/com/larrykin/Controllers/EditController.java
+++ src/main/java/com/larrykin/Controllers/EditController.java
@@ -26,7 +26,9 @@ import java.util.ResourceBundle;
 public class EditController implements Initializable {
 
     @FXML
-    private Button Search;
+    public AnchorPane editAnchorPane;
+    @FXML
+    private Button searchButton;
 
     @FXML
     private AnchorPane addAnchorPane;
@@ -65,17 +67,21 @@ public class EditController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         ComboBoxUtils.populateLanguageComboBox(languageComboBox);
-        ComboBoxUtils.populateLanguageComboBox(milestoneComboBox);
+        ComboBoxUtils.populateMilestoneComboBox(milestoneComboBox);
         List<String> projectNames = fetchAllProjectNames();
         chooseProjectComboBox.setItems(FXCollections.observableArrayList(projectNames));
 
-        Search.setOnAction(event -> {
-            searchProject();
+        searchButton.setOnAction(event -> {
+            String selectedProjectName = chooseProjectComboBox.getValue();
+            if (selectedProjectName == null) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Please select a project");
+                alert.showAndWait();
+            }else{
+                searchProject(selectedProjectName);
+            }
         });
-        updateButton.setOnAction(event -> {
-            getProject();
-        });
-
     }
 
 
@@ -97,81 +103,116 @@ public class EditController implements Initializable {
     }
 
     //? Method to search for a project
-    private Project searchProject() {
-        String selectedProjectName = chooseProjectComboBox.getValue();
-        if (selectedProjectName == null) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Please select a project");
-            alert.showAndWait();
-            return null;
-        }
+    private Project searchProject(String selectedProjectName) {
+
+
 
         Project foundProject = null;
         String query = "SELECT * FROM projects WHERE project_name = ?";
-        try (Connection conn = connectNow.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
+        try (
+                Connection conn = connectNow.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
             pstmt.setString(1, selectedProjectName);
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
                     foundProject = new Project();
+                    //?populate the fields with the project details
+                    if (foundProject != null) {
+                        try {
+
+                            System.out.println("starting to populate the fields in searchProject method");
+
+                            //?populate the fields with the project details
+                            String date = rs.getString("date");
+                            String projectName = rs.getString("project_name");
+                            String language = rs.getString("language");
+                            String projectDescription = rs.getString("project_description");
+                            String futureImprovements = rs.getString("future_improvements");
+                            String milestone = rs.getString("milestone");
+                            String milestoneDesc = rs.getString("milestone_description");
+
+
+                            datePicker.setValue(LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                            chooseProjectComboBox.setValue(projectName);
+                            languageComboBox.setValue(language);
+                            projectDescriptionTextArea.setText(projectDescription);
+                            futureImprovementTextArea.setText(futureImprovements);
+                            milestoneComboBox.setValue(milestone);
+                            milestoneDescription.setText(milestoneDesc);
+
+
+                            //? Keep track of the current project
+                            currentProject = foundProject;
+
+                            //? Enable the update button
+                            updateButton.setDisable(false);
+
+                            updateButton.setOnAction(event -> {
+                                updateProjectInDatabase();
+                            });
+                        } catch (Exception e) {
+                            System.out.println("Error updating nodes values from searchProject: " + e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+
                 }
+            } catch (Exception e) {
+                System.out.println("Error in searchProject method: " + e.getMessage());
+                e.printStackTrace();
             }
-            //? Enable the update button
-            updateButton.setDisable(false);
+
         } catch (SQLException e) {
+            System.out.println("Error searching for project: " + e.getMessage());
             e.printStackTrace();
         }
         return foundProject;
     }
 
-    private Project getProject() {
-        //? get the return value or the searchProject method
-        Project project = searchProject();
-        updateProject(project);
-        return null;
-    }
 
     //? Method to perfonm the update
     public void updateProject(Project project) {
         if (project != null) {
-            try{
+            try {
 
-            //?populate the fields with the project details
-            datePicker.setValue(LocalDate.parse(project.getDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-            chooseProjectComboBox.setValue(project.getProjectName());
-            languageComboBox.setValue(project.getLanguage());
-            projectDescriptionTextArea.setText(project.getProjectDescription());
-            futureImprovementTextArea.setText(project.getFutureImprovements());
-            milestoneComboBox.setValue(project.getMilestone());
-            milestoneComboBox.setValue(project.getMilestoneDescription());
-            milestoneDescription.setText(project.getMilestoneDescription());
+                System.out.println("starting to populate the fields in updateProject method");
+                //?populate the fields with the project details
+                datePicker.setValue(LocalDate.parse(project.getDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                chooseProjectComboBox.setValue(project.getProjectName());
+                languageComboBox.setValue(project.getLanguage());
+                projectDescriptionTextArea.setText(project.getProjectDescription());
+                futureImprovementTextArea.setText(project.getFutureImprovements());
+                milestoneComboBox.setValue(project.getMilestone());
+                milestoneComboBox.setValue(project.getMilestoneDescription());
+                milestoneDescription.setText(project.getMilestoneDescription());
 
-            //? Keep track of the current project
-            currentProject = project;
+                //? Keep track of the current project
+                currentProject = project;
 
-            //? save the updated project to the database
-            updateProjectInDatabase();
+                //? save the updated project to the database
+                updateProjectInDatabase();
 
-            }catch (Exception e){
-                System.out.println("Error updating nodes values: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Error updating nodes values from updateProject: " + e.getMessage());
                 e.printStackTrace();
             }
         }
     }
 
     private void updateProjectInDatabase() {
+        System.out.println("inside updateProjectInDatabase method");
         if (currentProject == null) {
             return;
         }
         try {
+            System.out.println("inside try block of updateProjectInDatabase method");
             Connection connectDB = connectNow.getConnection(); //! try with resources closes the connection automatically
             String query = "UPDATE projects SET project_name = ?, date = ?, language = ?, project_description = ?, future_improvements = ?, milestone = ?, milestone_description = ? WHERE project_name = ?";
 
             PreparedStatement pstmt = connectDB.prepareStatement(query);
 
             pstmt.setString(1, chooseProjectComboBox.getValue());
-            pstmt.setString(2, datePicker.getValue().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+            pstmt.setString(2, datePicker.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
             pstmt.setString(3, languageComboBox.getValue());
             pstmt.setString(4, projectDescriptionTextArea.getText());
             pstmt.setString(5, futureImprovementTextArea.getText());
@@ -181,17 +222,17 @@ public class EditController implements Initializable {
 
             int rowAffected = pstmt.executeUpdate();
             pstmt.close();
-            if(rowAffected > 0) {
+            if (rowAffected > 0) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Success");
                 alert.setHeaderText("Project updated successfully");
 
-                Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(2), event-> alert.close()));
+                Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(2), event -> alert.close()));
                 timeline.setCycleCount(1);
                 timeline.play();
 
                 alert.showAndWait();
-            }else{
+            } else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("Error updating project");
